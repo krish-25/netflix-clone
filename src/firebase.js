70a907574811654ae0ee8583c085ca064b8b658f
unknown <abhishekkrishan02@gmail.com>
2021-05-07 16:54:24 +0530

    deployed on firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuOPbOEr0ZQM8bbsxhnurHIC4qeWLJd0k",
+    authDomain: "netflix-clone-adae1.firebaseapp.com",
+    projectId: "netflix-clone-adae1",
+    storageBucket: "netflix-clone-adae1.appspot.com",
+    messagingSenderId: "824434660052",
+    appId: "1:824434660052:web:c824c78bb8f93929412141",
+    measurementId: "G-X3G9Z5SRZB"
+  };
